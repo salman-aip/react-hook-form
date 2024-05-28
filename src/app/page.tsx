ab24f2382d856bbddc737d9d409b793dfb9c42d3
salman-aip <avenirsalman@gmail.com>
2024-05-28 17:26:30 +0600

    form with nested object and array values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FormWithErrorMessage from "@/components/form-with-error-message";
+import FormWithNestedObjectAndArray from "@/components/form-with-nested-object-and-array";
 import FormWithRegisterValidation from "@/components/form-with-register-validation";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-white text-black">
       {/* <FormWithRegisterValidation /> */}
-      <FormWithErrorMessage />
+      {/* <FormWithErrorMessage /> */}
+      <FormWithNestedObjectAndArray />
     </main>
   );
 }
